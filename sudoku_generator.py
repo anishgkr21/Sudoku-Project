52967efcc1463d8f06e6c7c2ec1008b80bf7dbaf
anishgkr21 <166748013+anishgkr21@users.noreply.github.com>
2024-04-26 23:24:20 -0400

    init_board function created more code added

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -121,7 +121,14 @@ class SudokuGenerator:
 	Return: boolean
     '''
     def valid_in_box(self, row_start, col_start, num):
-        pass
+        rows= row_start//self.box_length
+        columns= col_start//self.box_length
+        for i in range(self.box_length):
+            for j in range(self.box_length):
+                if self.board[rows*3+i][columns*3+j] == num:
+                    return False
+        return True
+
     
     '''
     Determines if it is valid to enter num at (row, col) in the board
@@ -134,7 +141,14 @@ class SudokuGenerator:
 	Return: boolean
     '''
     def is_valid(self, row, col, num):
-        pass
+        if not self.valid_in_box(row,num):
+            return False
+        if not self.valid_in_box(col,num):
+            return False
+        if not self.valid_in_box(row, col, num):
+            return False
+        return True
+
 
     '''
     Fills the specified 3x3 box with values
@@ -147,7 +161,12 @@ class SudokuGenerator:
 	Return: None
     '''
     def fill_box(self, row_start, col_start):
-        pass
+        numbers= [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        for j in range(row_start, row_start+3):
+            for i in range(col_start, col_start+3):
+                random = random.randint(0, len(numbers))
+                self.board[j][i] = numbers[random-1]
+                numbers.pop(random-1)
     
     '''
     Fills the three boxes along the main diagonal of the board
